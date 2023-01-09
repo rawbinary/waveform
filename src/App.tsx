ac5217ed9cc15213ea5855accf6fa9474306a6bb
Robin Panta <hacktivistic@gmail.com>
2023-01-09 23:31:48 +0530

    add audio player

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,33 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
+import { ChangeEvent } from "react";
 import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0);
+  if (!window.AudioContext) {
+    return alert("You got a bad browser.");
+  }
+
+  const handleFileSelect = function (e: ChangeEvent<HTMLInputElement>) {
+    const audioFile = e.target.files?.item(0);
+    if (!audioFile) return alert("Not a valid file selected.");
+
+    const audio = document.querySelector("#audio") as HTMLAudioElement;
+    audio.src = URL.createObjectURL(audioFile);
+    audio.play();
+  };
 
   return (
     <div className="App">
       <h1>Waveform Visualizer</h1>
-      <div className="card"></div>
-      <p className="read-the-docs">Select a file to visualize its waveform.</p>
+      <p>Select a file to visualize its waveform.</p>
+      <div className="card">
+        <input
+          type="file"
+          id="audioFile"
+          onChange={(e) => handleFileSelect(e)}
+          accept="audio/*"
+        />
+        <audio id="audio" controls />
+      </div>
     </div>
   );
 }
