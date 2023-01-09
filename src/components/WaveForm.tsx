4341a7079c76ba566301d1bc3ea1a7a08bb3798f
Robin Panta <hacktivistic@gmail.com>
2023-01-10 03:27:34 +0530

    sinewave visualization

--- src/components/WaveForm.tsx
+++ src/components/WaveForm.tsx
@@ -1,16 +1,56 @@
 import { useEffect, useRef } from "react";
 import useSize from "../hooks/useSize";
 
-export default function WaveForm({ data }: WaveFormProps) {
+export default function WaveForm({ analyser }: WaveFormProps) {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const [width, height] = useSize();
 
   useEffect(() => {
-    const ctx = canvasRef?.current?.getContext("2d");
-    if (!ctx) return;
+    const canvas = canvasRef.current;
+    if (!canvas) return;
 
-    //
-  }, []);
+    if (!analyser) return;
+
+    const canvasCtx = canvas.getContext("2d") as CanvasRenderingContext2D;
+
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    analyser.getByteTimeDomainData(dataArray);
+
+    const animate = () => {
+      requestAnimationFrame(animate);
+
+      analyser.getByteTimeDomainData(dataArray);
+
+      canvas.width = canvas.width;
+      canvasCtx.translate(0, canvas.offsetHeight / 2); // Set Y = 0 to be in the middle of the canvas
+      canvasCtx.lineWidth = 3;
+      canvasCtx.strokeStyle = "rgb(255, 255, 255)";
+
+      canvasCtx.beginPath();
+
+      const sliceWidth = (canvas.width * 1.0) / bufferLength;
+      let x = 0;
+
+      for (let i = 0; i < bufferLength; i++) {
+        const v = dataArray[i] / 255;
+        const y = (v * canvas.offsetHeight) / 2;
+
+        if (i === 0) {
+          canvasCtx.moveTo(x, y);
+        } else {
+          canvasCtx.lineTo(x, y);
+        }
+
+        x += sliceWidth;
+      }
+
+      canvasCtx.lineTo(canvas.width + 10, canvas.offsetHeight / 2);
+      canvasCtx.stroke();
+    };
+
+    animate();
+  }, [analyser]);
 
   return (
     <>
@@ -25,5 +65,12 @@ export default function WaveForm({ data }: WaveFormProps) {
 }
 
 type WaveFormProps = {
-  data: number[];
+  analyser: AnalyserNode | undefined;
 };
+
+function setup(analyser: AnalyserNode) {
+  analyser.fftSize = 2048;
+
+  const bufferLength = analyser.frequencyBinCount;
+  const dataArray = new Uint8Array(bufferLength);
+}
