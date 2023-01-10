5df61a2b258d88cca07fc459662369773d622060
Robin Panta <hacktivistic@gmail.com>
2023-01-11 02:51:41 +0530

    remove audio context from player hook

--- src/hooks/useAudioContext.ts
+++ src/hooks/useAudioContext.ts
@@ -1,11 +1,9 @@
 import { createContext, useContext } from "react";
 
 export const AudioPlayerContext = createContext<{
-  ctx: AudioContext;
   analyser: AnalyserNode | undefined;
   playing: boolean;
 }>({
-  ctx: new AudioContext(),
   analyser: undefined,
   playing: false,
 });
